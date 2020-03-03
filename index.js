f93a4be8c0c36a3d4fb04fc566d781b67c086ab4
emily <mle4d@me.com>
2020-03-02 17:44:33 -0800

    JS for al modals

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 var ogModal = document.getElementById("ogModal");
 var ogbtn = document.getElementById("ogBtn");
+var mintModal = document.getElementById("mintModal");
+var mintbtn = document.getElementById("mintBtn");
+var creepModal = document.getElementById("creepModal");
+var creepbtn = document.getElementById("creepBtn");
+var shortsModal = document.getElementById("shortsModal");
+var shortsbtn = document.getElementById("shortsBtn");
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+var span4 = document.getElementsByClassName("close")[3];
 
 ogbtn.onclick = function() {
   ogModal.style.display = "block";
@@ -25,7 +34,52 @@ span.onclick = function() {
   ogModal.style.display = "none";
 }
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target == ogModal) {
     ogModal.style.display = "none";
   }
 }
+mintbtn.onclick = function() {
+  mintModal.style.display = "block";
+}
+span2.onclick = function() {
+  mintModal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    mintModal.style.display = "none";
+  }
+}
+creepbtn.onclick = function() {
+  creepModal.style.display = "block";
+}
+span3.onclick = function() {
+  creepModal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    creepModal.style.display = "none";
+  }
+}
+shortsbtn.onclick = function() {
+  shortsModal.style.display = "block";
+}
+span4.onclick = function() {
+  shortsModal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    shortsModal.style.display = "none";
+  }
+}
+// span.onclick = function() {
+//   ogModal.style.display = "none";
+// }
+// span2.onclick = function() {
+//   mintModal.style.display = "none";
+// }
+// span3.onclick = function() {
+//   creepModal.style.display = "none";
+// }
+// span4.onclick = function() {
+//   shortsModal.style.display = "none";
+// }
